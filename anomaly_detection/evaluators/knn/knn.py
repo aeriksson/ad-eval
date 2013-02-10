06d03ccd8970a3fc95f41a29b140140e0be65900
aeriksson <andree88@gmail.com>
2013-02-10 08:52:57 -0800

    made kNN return NaN instead of crashing if the reference set is too small

--- anomaly_detection/evaluators/knn/knn.py
+++ anomaly_detection/evaluators/knn/knn.py
@@ -1,25 +1,26 @@
-'''
-k-Nearest Neighbors evaluator.
-
-Currently uses brute force to find the kNN distnce - this is the best
-we can do without prior information about the distance function.
-
-TODO: use ANN for approximate Minkowski kNN.
-'''
-
 import heapq
 
 
 class KNNEvaluator(object):
+    '''
+    k-Nearest Neighbors evaluator.
 
-    _training_series = []
+    Currently uses brute force to find the kNN distnce - this is the best
+    we can do without extra information about the distance function.
+    '''
 
     def __init__(self, distance, k=3):
         self._distance = distance
         self._k = k
 
     def evaluate(self, evaluation_series, reference_set, *args):
+
         distances = [self._distance(evaluation_series, s) for s in reference_set]
+
+        # return NaN if there are not enough elements in the reference set
+        if len(distances) < self._k:
+            return float('NaN')
+
         d = heapq.nsmallest(self._k, distances)[self._k - 1]
         return d
 
