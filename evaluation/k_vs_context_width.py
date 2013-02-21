4ea4cc1a6215c8537fbe4daf6cc4092c7acc59fc
André Eriksson <andree88@gmail.com>
2013-02-21 19:01:58 +0100

    Minor changes, added anomaly vector plot

--- evaluation/k_vs_context_width.py
+++ evaluation/k_vs_context_width.py
@@ -1,18 +1,21 @@
-import anomaly_detection
-import utils
-import random
-
-import defaults
-
 """
 Evaluates a test on a kNN anomaly detector with different values
 of k and window width and creates a heat map of the accuracy.
 """
+import anomaly_detection
+import utils
+import random
+from matplotlib import pyplot
 
-rng = random.Random()
+import defaults
 
-K_VALUES = range(1, 100, 10)
-W_VALUES = range(1, 100, 10)
+import pickle
+
+rng = random.Random(4)
+
+K_VALUES = range(1, 50, 1)
+W_VALUES = range(1, 50, 1)
+TEST_FILE = 'sequences/random_walk_added_noise'
 
 # generate anomaly_detectors
 ad_config = defaults.DEFAULT_KNN_CONFIG
@@ -29,20 +32,43 @@ for k in K_VALUES:
         ad_row.append(str(k) + ',' + str(w))
     ad_label_matrix.append(ad_row)
 
-# generate test
-test_config = {'count': 1, 'amplitude_interval': [0.0, 1.0], 'width_interval': [5, 40], 'anomaly_type': 'noise'}
-base_sequence = utils.generate_bridged_random_walk(400, 0, 0.1, rng)
-test = utils.generate_random_test(base_sequence, random_number_generator=rng, **test_config)
+ad_label_list = sum(ad_label_matrix, [])
 
-test_suite = utils.TestSuite(anomaly_detectors, sum(ad_label_matrix, []), [test], 'test')
+test = [utils.load_sequence(TEST_FILE)]
+test_suite = utils.TestSuite(anomaly_detectors, ad_label_list, [test], 'test')
 
 #execute
-test_suite.evaluate(display_progress=True)
+#test_suite.evaluate(display_progress=True)
 
 results = test_suite.results
 
-fig, plot = results.get_average_value_plot(ad_label_matrix, K_VALUES, W_VALUES, 'equal_support_distance')
-plot.set_xlabel('k')
-plot.set_ylabel('w')
+f = open('results.tmp', 'r+')
+#pickle.dump(results, f)
+results = pickle.load(f)
+f.close()
+
+# plot the distances
+ERRORS = ['equal_support_distance', 'full_support_distance', 'normalized_euclidean_distance']
+ERROR_LABELS = ['Equal support', 'Full support', 'Normalized Euclidean']
+
+for error in ERRORS:
+    utils.plots.plot_average_value_heat_map(results, ad_label_matrix, K_VALUES,
+                                            W_VALUES, error, 'k', 'w')
+
+
+# plot the anomaly vectors which minimize the distance measures
+def get_label_of_n_smallest_key_value(distance_key, n):
+    avgs = results.get_anomaly_detector_averages(ad_label_list, distance_key)
+    sorted_avgs = sorted(zip(avgs, ad_label_list), key=lambda (a, b): a)
+    return sorted_avgs[n][1]
+
+
+fig = pyplot.figure()
+plots = [fig.add_subplot(2, 2, i + 1) for i in range(4)]
+ns = [1, 10, 50, 100]
+for n, plot in zip(ns, plots):
+    mins = [get_label_of_n_smallest_key_value(x, n) for x in ERRORS]
+    title = "n = %s" % n
+    utils.plots.plot_anomaly_vectors(results, mins, ERROR_LABELS, title=title, plot=plot, normalize=True)
 
 fig.show()
